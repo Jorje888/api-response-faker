5e7a1f80ac5d3661d5ac52242002c197b864cb8f
c8nst <kostiaferg@gmail.com>
2025-06-03 22:43:49 +0400

    added the basis of manager

---
+++ backend/src/routes/ruleManager.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { pool } from '../db/db';
+import { FakeApiRule } from '../types';
+
+const router = express.Router();
+
+// Route to get all rules
+
+export default router;
