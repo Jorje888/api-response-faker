b493a1a87c5b98bf4e1be7e61e6ed86e7aea69a2
c8nst <kostiaferg@gmail.com>
2025-06-12 22:13:22 +0400

    this was the useless rudiment

--- backend/src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface FakeApiRule {
-    id: number;
-    path: string;
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
-    statusCode: number;
-    contentType: string;
-    responseBody: string; 
-}
