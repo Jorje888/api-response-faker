75e3a0eb5756e058d609de09efd533ae0ce6ca3f
Dachi Tatunashvili <tatunashvilidachi@gmail.com>
2025-06-13 17:21:02 +0400

    changed types (basically took the ones that were from the backend , so now I will have to change some structures in my code to adjust perfectly with backend)

--- frontend/src/RuleList.tsx
+++ frontend/src/RuleList.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import "./RuleList.css";
 
+
+export enum HttpMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  PATCH = "PATCH",
+  HEAD = "HEAD",
+  OPTIONS = "OPTIONS",
+  ALL = "ALL",
+}
+export enum ContentType {
+  JSON = "application/json",
+  TEXT = "text/plain",
+  HTML = "text/html",
+  XML = "application/xml",
+}
 export interface Rule {
-    id: number;
-    name: string;
-    path: string;
-    method: string;
-    statusCode: number;
-    contentType: string;
-    responseBody: string;
+  id: number;
+  name: string; // Assuming 'name' is still needed for display purposes in RuleList
+  path: string;
+  method: HttpMethod; // Use the HttpMethod enum
+  statusCode: number;
+  contentType: ContentType; // Use the ContentType enum
+  responseBody: string;
 }
 
-interface RuleListarr{
-    rules: Rule[];
+
+
+interface RuleListarr {
+  rules: Rule[];
 }
 
 function RuleList({ rules }: RuleListarr) {
