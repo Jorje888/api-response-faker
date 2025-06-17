cfe79e8d6d11457c68d232fd6204f90fe693ee8d
Dachi Tatunashvili <tatunashvilidachi@gmail.com>
2025-06-18 00:08:15 +0400

    make beautiful style of css

--- frontend/src/LoginPage.tsx
+++ frontend/src/LoginPage.tsx
@@ -1,19 +1,26 @@
+import styles from './LoginPage.module.css';
+import { Link } from 'react-router-dom';
 function LoginPage() {
   return (
-    <div>
+      <div className={styles.container}> 
+       <div className={styles.formContainer}> 
       <h2>Login Page</h2>
       <p>fill in the form of login please.</p>
       <form>
-        <div>
+        <div className={styles.formGroup}>
           <label>Email: </label>
           <input type="email" />
         </div>
-        <div>
+        <div className={styles.formGroup}>
           <label>Password: </label>
           <input type="password" />
         </div>
         <button type="submit">Log In</button>
+         <Link to="/register" className={styles.formLink}>
+          Don't have an account? Register
+        </Link>
       </form>
+      </div>
     </div>
   );
 }
--- frontend/src/RegisterPage.tsx
+++ frontend/src/RegisterPage.tsx
@@ -1,21 +1,37 @@
+import { Link } from 'react-router-dom';
+import styles from './RegisterPage.module.css'; // 1. Import the CSS module
+
 function RegisterPage() {
   return (
-    <div>
+    <div className={styles.formContainer}>
+       <div className={styles.formContainer}>
       <h2>Register Page</h2>
       <p>please fill in the forms given below</p>
        <form>
-        <div>
+        <div className={styles.formGroup}>
           <label>Email: </label>
           <input type="email" />
         </div>
-        <div>
+        <div className={styles.formGroup}>
           <label>Password: </label>
           <input type="password" />
         </div>
         <button type="submit">Register</button>
       </form>
+      <Link to="/login" className={styles.formLink}>
+        Already have an account? Login
+      </Link>
+      </div>
     </div>
   );
 }
+
+
+      <div className={styles.formContainer}>
+
+
+
+
+      </div>
 
 export default RegisterPage;
