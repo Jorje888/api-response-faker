570f7372ce8ba9c3882dc2ff159f78f6d01b6346
Dachi Tatunashvili <tatunashvilidachi@gmail.com>
2025-06-17 18:27:48 +0400

    created socket to give the info that will be fetched from the frontend to the backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,52 @@ import RuleForm from './RuleForm';
 import RuleList from './RuleList';
 import type {Rule} from './RuleList';
 import { useState } from 'react';
+import socket from './socket';
+import { useEffect } from 'react';
 
+
+// ამ კოდს შევცვლი იმის მიხედვით როგორ მომიწევს გატესტვა , საიდან რისი ჩამოტვირთვა მომიწევს ,
+// სოკეტიდან როგორ , რა ბრძანებთ მიიღებენ ინფორმაციას
 
 function App() {
-  const [rules, setRules] = useState<Rule[]>([]);
+const [rules, setRules] = useState<Rule[]>([]);
+
+  useEffect(() => {
+    socket.connect();
+
+    socket.on('connect', () => {
+      console.log('Connected to server , dachi made it!' + socket.id);
+    });
+    socket.on('disconnect', (reason) => {
+      console.log(`Disconnected from server: ${reason}`);
+    });
+
+    socket.on('ruleAddedSuccess', (data: any) => {
+      setRules(prevRules => [...prevRules, data]);
+    });
+
+    socket.on('ruleAddError', (error: any) => {
+      error.printstacktrace(); // specific error details
+    });
+
+    return () => {
+      socket.disconnect();
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('ruleAddedSuccess');
+      socket.off('ruleAddError');
+    };
+  }, []);
+
   const handleAddRule = (newRule: Rule) => {
     setRules(prevRules => [...prevRules, newRule]);
+    socket.emit('addRule', newRule);
   };
 
   return (
     <div className='App'>
       <h1>API response faker</h1>
       <RuleForm onAddRule={handleAddRule} />
-      {/* Pass the 'rules' state variable */}
       <RuleList rules={rules} />
     </div>
   );
